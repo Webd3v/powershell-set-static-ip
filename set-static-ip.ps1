b9780075b10e35a4b16ab013f4a5245a85ddd546
Webd3v <padindanny@gmail.com>
2021-10-22 23:56:09 -0400

    Update set-static-ip.ps1

--- set-static-ip.ps1
+++ set-static-ip.ps1
@@ -1,8 +1,4 @@
-
 $Ipn = Read-Host -Prompt 'Input your IP address'
-
 $Sub = Read-Host -Prompt 'Input your subnet'
-
 $Gateway = Read-Host -Prompt 'Input your gateway'
-
 netsh interface ip set address name="Ethernet" static $Ipn $Sub $Gateway
